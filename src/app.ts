0fcb8f681264050528e405927fac58e767bb4841
lizi <qwq0833@hotmail.com>
2022-10-15 19:03:52 +0800

    feat(klines): Cache klines to json file

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,22 @@
 import express, { Request, Response } from 'express';
+import path from 'path';
+import fs from 'fs/promises';
 import KlinesService from './services/klines';
 
 const app = express();
 const port = 18700;
+
+/**
+ * 如果存放缓存文件的目录不存在则创建
+ */
+const initCacheDirectory = async () => {
+  const CACHE_DIR = path.join(__dirname, '..', 'cache');
+  try {
+    await fs.access(CACHE_DIR);
+  } catch (error) {
+    await fs.mkdir(CACHE_DIR);
+  }
+};
 
 app.get('/', (req: Request, res: Response) => {
   res.json({
@@ -13,5 +27,6 @@ app.get('/', (req: Request, res: Response) => {
 app.use('/klines', KlinesService);
 
 app.listen(port, () => {
+  initCacheDirectory();
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
 });
--- src/services/klines.ts
+++ src/services/klines.ts
@@ -1,8 +1,55 @@
 import { Router, Request, Response } from 'express';
 import axios, { AxiosError } from 'axios';
 import AdmZip from 'adm-zip';
+import path from 'path';
+import fs from 'fs/promises';
 
 const router = Router();
+
+/**
+ * 获取 K 线数据
+ * @param date 日期, 格式: YYYY-MM-DD
+ */
+const getKlines = async (date: string) => {
+  try {
+    const filename = `ETHBUSD-1m-${date}`;
+    const cachePath = path.join(__dirname, '..', '..', 'cache', `${filename}.json`);
+    let klines;
+
+    // 优先从缓存中读取
+    try {
+      const data = await fs.readFile(cachePath);
+      klines = JSON.parse(data.toString());
+    } catch (error) {
+      const remoteUrl = `https://data.binance.vision/data/spot/daily/klines/ETHBUSD/1m/${filename}.zip`;
+      const remoteZip = await axios.get(remoteUrl, {
+        responseType: 'arraybuffer'
+      });
+
+      // 从压缩包中读取数据
+      const zip = new AdmZip(remoteZip.data);
+      const csv = zip.getEntry(`${filename}.csv`);
+
+      // K 线数据不存在
+      if (!csv) {
+        return null;
+      }
+
+      const data = zip.readAsText(csv);
+      klines = data.split('\n').map(line => line.split(',').map(item => parseFloat(item)));
+
+      // 写到缓存文件
+      await fs.writeFile(cachePath, JSON.stringify(klines));
+    }
+
+    return klines;
+  } catch (error) {
+    if (error instanceof AxiosError && error.response?.status === 404) {
+      return null;
+    }
+    return Promise.reject(error);
+  }
+};
 
 router.get('/', async (req: Request, res: Response) => {
   const { from } = req.query;
@@ -11,30 +58,12 @@ router.get('/', async (req: Request, res: Response) => {
   }
 
   try {
-    const remoteUrl = `https://data.binance.vision/data/spot/daily/klines/ETHBUSD/1m/ETHBUSD-1m-${from}.zip`;
-    const remoteZip = await axios.get(remoteUrl, {
-      responseType: 'arraybuffer'
-    });
-
-    // 从压缩包中读取数据
-    const zip = new AdmZip(remoteZip.data);
-    const csv = zip.getEntry(`ETHBUSD-1m-${from}.csv`);
-
-    // K 线数据不存在
-    if (!csv) {
+    const klines = await getKlines(String(from));
+    if (!klines) {
       return res.status(404).json({ errMessage: 'Not Found' });
     }
-
-    const data = zip.readAsText(csv);
-    const klines = data.split('\n').map(line => line.split(',').map(item => parseFloat(item)));
-
-    res.json({
-      klines
-    });
+    return res.json({ klines });
   } catch (error) {
-    if (error instanceof AxiosError && error.response?.status === 404) {
-      return res.status(404).json({ errMessage: 'Not Found' });
-    }
     res.status(500).json({ errMessage: 'Internal Server Error' });
   }
 });
